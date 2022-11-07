5e14375bd65c6e69d16c8a5c3288a5e0bccc87ad
AntonKhliebnikov <towulik@gmail.com>
2022-11-07 23:28:53 +0200

    Module 11 fix Task5

--- src/main/java/module11/Task5.java
+++ src/main/java/module11/Task5.java
@@ -3,24 +3,26 @@ package module11;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task5 {
     public static void main(String[] args) {
-        Stream<Integer> first = Stream.of(1,9,3,87,45,11,65,42);
-        Stream<Integer> second = Stream.of(6,38,59,6,22,3);
+        Stream<Integer> first = Stream.of(1, 9, 3, 87, 45, 11, 65, 42);
+        Stream<Integer> second = Stream.of(6, 38, 59, 6, 22, 3);
 
         zip(first, second).forEach(System.out::println);
 
     }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        Iterator<T> iterator1 = first.iterator();
-        Iterator<T> iterator2 = second.iterator();
+        List<T> list1 = first.collect(Collectors.toList());
+        List<T> list2 = second.collect(Collectors.toList());
+        int minSize = Math.min(list1.size(), list2.size());
         List<T> result = new ArrayList<>();
-        while (iterator1.hasNext() && iterator2.hasNext()) {
-            result.add(iterator1.next());
-            result.add(iterator2.next());
+        for (int i = 0; i < minSize; i++) {
+            result.add(list1.get(i));
+            result.add(list2.get(i));
         }
         return result.stream();
     }
