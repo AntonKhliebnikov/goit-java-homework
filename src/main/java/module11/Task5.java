b7fd6674e15d14bd402b9c7e86c9f7a55cc5da83
AntonKhliebnikov <towulik@gmail.com>
2022-11-13 19:41:29 +0200

    Module 11 one more fix in Task5

--- src/main/java/module11/Task5.java
+++ src/main/java/module11/Task5.java
@@ -1,29 +1,27 @@
 package module11;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task5 {
     public static void main(String[] args) {
-        Stream<Integer> first = Stream.of(1, 9, 3, 87, 45, 11, 65, 42);
-        Stream<Integer> second = Stream.of(6, 38, 59, 6, 22, 3);
+        Stream<Integer> first = Stream.of(1, 9, 3, 87, 45, 12);
+        Stream<Integer> second = Stream.of(6, 38, 59, 6, 22, 3, 11, 64);
 
         zip(first, second).forEach(System.out::println);
 
     }
 
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        List<T> list1 = first.collect(Collectors.toList());
-        List<T> list2 = second.collect(Collectors.toList());
-        int minSize = Math.min(list1.size(), list2.size());
-        List<T> result = new ArrayList<>();
-        for (int i = 0; i < minSize; i++) {
-            result.add(list1.get(i));
-            result.add(list2.get(i));
-        }
-        return result.stream();
+        Iterator<T> iterator = first.iterator();
+        return second.flatMap(i ->
+        {
+            if (iterator.hasNext()) {
+                return Stream.of(i, iterator.next());
+            } else {
+                return Stream.empty();
+            }
+        });
     }
 }
